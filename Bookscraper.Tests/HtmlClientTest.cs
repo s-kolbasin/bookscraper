3c3286652889dc0f8523db19cbe91991f7e030dc
Stanislav Kolbasin <Stanislav.Kolbasin@wisetechglobal.com>
2024-01-31 00:42:13 +0100

    Combine path doesn't work for web

---
+++ Bookscraper.Tests/HtmlClientTest.cs
@@ -0,0 +1,28 @@
+﻿using BookScraper.Lib;
+
+namespace Bookscraper.Tests
+{
+	public class HtmlClientTest
+	{
+		private WebHtmlClient client = new ();
+
+		[Fact]
+		public void WhenSimpleLocalRef_AddsToCurrent()
+		{
+			AssertCombinesPath("books.toscrape.com", "index.html", "books.toscrape.com/index.html");
+		}
+
+		[Fact]
+		public void WhenLocalRefGoesUp_GoUp()
+		{
+			AssertCombinesPath("books.toscrape.com/travel", "../index.html", "books.toscrape.com/index.html");
+		}
+
+		private void AssertCombinesPath(string urlBase, string localRef, string expectedResult)
+		{
+			string fullUrl = client.CombinePath(urlBase, localRef);
+
+			Assert.Equal(expectedResult, fullUrl);
+		}
+	}
+}
