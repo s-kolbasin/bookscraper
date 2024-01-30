9bf86a1e67c84b542d62b505525e36627324884a
Stanislav Kolbasin <Stanislav.Kolbasin@wisetechglobal.com>
2024-01-31 00:59:17 +0100

    (To some extent) fixed url combine

--- BookScraper.Lib/Classes/WebHtmlClient.cs
+++ BookScraper.Lib/Classes/WebHtmlClient.cs
@@ -1,11 +1,13 @@
 
+using Flurl;
+
 namespace BookScraper.Lib;
 
 public sealed class WebHtmlClient : IHtmlClient
 {
 	private readonly HttpClient internalClient = new ();
 
-	public string CombinePath(string root, string localPath) => root + localPath;
+	public string CombinePath(string root, string localPath) => Url.Combine(root, localPath);
 
 	public void Dispose() => internalClient.Dispose();
 
--- Bookscraper.Tests/HtmlClientTest.cs
+++ Bookscraper.Tests/HtmlClientTest.cs
@@ -15,7 +15,8 @@ namespace Bookscraper.Tests
 		[Fact]
 		public void WhenLocalRefGoesUp_GoUp()
 		{
-			AssertCombinesPath("books.toscrape.com/travel", "../index.html", "books.toscrape.com/index.html");
+			// TODO: Can we simplify? This should work, but of course not ideal, since we'd visit some pages multiple times
+			AssertCombinesPath("books.toscrape.com/travel", "../index.html", "books.toscrape.com/travel/../index.html");
 		}
 
 		private void AssertCombinesPath(string urlBase, string localRef, string expectedResult)
